62b87ab12a2a1d9c9c644279806d253275afe88f
EmilyDSarani <emily.sarani@gmail.com>
2021-12-09 16:21:48 -0600

    add page is almost done

--- src/views/Teams/TeamAdd.jsx
+++ src/views/Teams/TeamAdd.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function TeamAdd() {
+    const [name, setName] = useState('');
+    const [city, setCity] = useState('');
+    const[state,setState] = useState('');
+
+//for is to htmlFor as class is to className, 
     return (
+        <>
         <div>
-            <h1>Add Team</h1>
+            <h1>Register Your Team!</h1>
         </div>
+        <fieldset>
+            <form>
+                <label htmlFor='name'>Name:</label>   
+                <input id='name' name='name' type='text' />
+                <label htmlFor='city'>City:</label>   
+                <input id='city' name='city' type='text' />
+                <label htmlFor='state'>State:</label>   
+                <input id='state' name='state' type='text' />
+
+                <input type="submit" value="Add your Team" />
+            </form>
+        </fieldset>
+        </>
     )
 }
